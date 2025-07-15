0ae4d512209f75e75c7f27c3f893753cf50ff571
Vladimir Solovyov <leilyngale@icloud.com>
2025-07-15 22:54:00 +0800

    Scaffold the API and client app, add docker compose to spin up postgres DB

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button, Typography } from "@mui/material";
+
+
+export default function App() {
+  
+
+  return (
+    <>
+      <Typography variant="body1">
+        Hey there
+      </Typography>
+      <Button variant="contained">Here</Button>
+    </>
+  )
+}
+
