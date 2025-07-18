3371999ef7937a915a8dfa6c58b152c5377a6ca4
Vladimir Solovyov <leilyngale@icloud.com>
2025-07-18 11:12:58 +0800

    Add LibraryServices and Footer components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css'
+import Footer from './layouts/Footer'
 import Carousel from './layouts/homepage/carousel/Carousel'
 import ExploreTopBooks from './layouts/homepage/ExploreTopBooks'
 import Hero from './layouts/homepage/Hero'
+import LibraryServices from './layouts/homepage/LibraryServices'
 import Navbar from './layouts/Navbar'
 
 
@@ -14,6 +16,8 @@ export default function App() {
       <ExploreTopBooks />
       <Carousel />
       <Hero />
+      <LibraryServices />
+      <Footer />
     </>
   )
 }
